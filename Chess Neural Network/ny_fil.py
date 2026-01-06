571a117792444e011ae4df6ac15a334e710120a2
nolegs123 <mikkelbaunchristensen@gmail.com>
2026-01-06 10:41:20 +0100

    Added relative paths to PGN files (Needs to be changed to be fixed) Created a function to read games from PGN file Function opens entire PGN file and goes through each move in a game as long as a game exists and prints FEN strings for each position

--- Chess Neural Network/ny_fil.py
+++ Chess Neural Network/ny_fil.py
@@ -1,18 +1,25 @@
-message = input("Write the word 'hello' in your native language").lower()
-if message == "hej":
-    print("HEJ")
-elif message == "hello":
-    print("HELLO")
-elif message == "hola":
-    print("HOLA")
-elif message == "aloha":
-    print("ALOHA")
-elif message == "bonjour":
-    print("BONJOUR")
-elif message == "hallo":
-    print("HALLO")
-elif message == "witaf":
-    print("WITAF")
-else:
-    print("BRUH MOMEMENT")
+import os
+import chess.pgn
 
+# Relative paths to datasets
+current_directory_name = os.path.dirname(__file__)
+all_ratings_directory = os.path.join(current_directory_name, 'lichess_all_ratings.pgn')
+elite_directory = os.path.join(current_directory_name, 'lichess_elite.pgn')
+
+def read_games_from_pgn(directory: str, shard_size: int = 10000, print_progress: bool = False, games_per_print: int = 0):
+    pgn = open(directory) # Opens entire PGN
+    shard_id = 0 # Shard ID
+    fen_number = 0 # Amount of FENs saved
+
+    while True:
+        game = chess.pgn.read_game(pgn) # Sets game to current game
+        if game is None:
+            break # Breaks loop if no game
+
+        board = game.board()
+
+        for move in game.mainline_moves(): # Goes through all moves in each game
+            board.push(move)
+            print(board.fen())
+
+read_games_from_pgn(all_ratings_directory) 
