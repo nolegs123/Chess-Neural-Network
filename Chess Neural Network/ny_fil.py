15df262537584d2455f6780d8938ad02372b57cf
nolegs123 <mikkelbaunchristensen@gmail.com>
2026-01-06 15:12:43 +0100

    Analyzed games and created 10 million FEN strings

--- Chess Neural Network/ny_fil.py
+++ Chess Neural Network/ny_fil.py
@@ -1,17 +1,13 @@
 import os
 import chess.pgn
 
-# Relative paths to datasets
-current_directory_name = os.path.dirname(__file__)
-all_ratings_directory = os.path.join(current_directory_name, 'lichess_all_ratings.pgn')
-elite_directory = os.path.join(current_directory_name, 'lichess_elite.pgn')
+all_ratings_directory = r"C:\Users\mikke\Downloads\lichess_elite_2021-08\lichess_elite_2021-08.pgn"
 
-def read_games_from_pgn(directory: str, shard_size: int = 10000, print_progress: bool = False, games_per_print: int = 0):
+def read_games_from_pgn(directory: str, shard_size: int = 10000):
     pgn = open(directory) # Opens entire PGN
     shard_id = 0 # Shard ID
     fen_number = 0 # Amount of FENs saved
 
-    
     while True:
         game = chess.pgn.read_game(pgn) # Sets game to current game
         if game is None:
@@ -19,14 +15,17 @@ def read_games_from_pgn(directory: str, shard_size: int = 10000, print_progress:
 
         board = game.board()
 
-        with open("FEN_moves.txt", "a") as file: # Creates FEN_moves.txt file if it doesn’t exist.
+        for move in game.mainline_moves(): # Goes through all moves in each game
+            with open(f"FENs/FEN_moves_{shard_id}.txt", "a") as file: # Creates FEN_moves{shard_id}.txt file if it doesn’t exist.
+                board.push(move) # Plays move
+                fen = board.fen() # Translates board to FEN string
 
-            for move in game.mainline_moves(): # Goes through all moves in each game
-                board.push(move)
+                fen_number += 1
 
-                print(board.fen())
+                if fen_number % shard_size == 0: # Move to the next shard once the current shard reaches shard_size entries
+                    shard_id += 1
 
-                file.write(f"{board.fen()}\n") # Append each FEN move from each game to file
+                file.write(f"{fen}\n") # Append each FEN move from each game to file
 
 read_games_from_pgn(all_ratings_directory) 
 
