8b64b658c447a76eada1c6c7ce0671aa75c52215
MischievousDegenerate <runersc@outlook.com>
2026-01-06 15:53:56 +0100

    new file update

--- Chess Neural Network/stockfishtraining.py
+++ Chess Neural Network/stockfishtraining.py
@@ -3,9 +3,27 @@ import chess.engine
 
 engine = chess.engine.SimpleEngine.popen_uci("/Users/dag/Desktop/DTU/02461_introduction_to_intelligent_systems/projectwork/stockfish/stockfish-macos-m1-apple-silicon")
 
-board = chess.Board("4q1Rn/3pQ3/5N2/5p1r/1bp1n2k/8/3P3P/K6R w - - 0 1")
-while not board.is_game_over():
-    result = engine.play(board, chess.engine.Limit(time=0.1))
-    board.push(result.move)
+
+
+for shard_id in range(1000):
+    with open(f"FENs/FEN_moves_{shard_id}.txt", "r") as file:
+        FENs = file.readlines()
+
+        with open(f"FENs_optimal_moves/FEN_optimal_move_{shard_id}.txt", "a") as f:
+
+            for FEN in FENs:
+                board = chess.Board(FEN)
+
+                # analyze move
+                info = engine.analyse(board, chess.engine.Limit(depth=12))
+                move = info["pv"][0]
+
+                f.write(f"{FEN} | {move}\n")
+
+
+
+
+    
+    
 
 engine.quit()
