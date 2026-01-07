8ec0a392a7d3123a488db350de25a15f55f5aba6
nolegs123 <mikkelbaunchristensen@gmail.com>
2026-01-07 10:00:36 +0100

    Moved function up to make program work

--- Chess Neural Network/stockfishtraining.py
+++ Chess Neural Network/stockfishtraining.py
@@ -2,27 +2,6 @@ import chess
 import chess.engine
 import random
 
-engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\mikke\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe")
-for shard_id in range(1000):
-    with open(f"FENs/FEN_moves_{shard_id}.txt", "r") as file:
-        FENs = file.readlines()
-
-        with open(f"FENs_optimal_moves/FEN_optimal_move_{shard_id}.txt", "a") as f:
-
-            for FEN in FENs:
-                FEN = FEN.strip()
-                board = chess.Board(FEN)
-
-                if board.is_game_over():
-                    board = generate_board(random.randint(0, 100))
-                
-                # analyze move
-                info = engine.analyse(board, chess.engine.Limit(depth=12))
-                move = info["pv"][0] # Choose highest ranked move
-                
-
-                f.write(f"{FEN} | {move}\n") # append the (FEN | optimal_move)
-
 def generate_board(move_amount: int) -> str:
     board = chess.Board()
 
@@ -43,5 +22,25 @@ def generate_board(move_amount: int) -> str:
 
     return board.fen()
 
+engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\mikke\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe")
+for shard_id in range(1000):
+    with open(f"FENs/FEN_moves_{shard_id}.txt", "r") as file:
+        FENs = file.readlines()
+
+        with open(f"FENs_optimal_moves/FEN_optimal_move_{shard_id}.txt", "a") as f:
+
+            for FEN in FENs:
+                FEN = FEN.strip()
+                board = chess.Board(FEN)
+
+                if board.is_game_over():
+                    board = chess.Board(generate_board(random.randint(0, 100)))
+                
+                # analyze move
+                info = engine.analyse(board, chess.engine.Limit(depth=12))
+                move = info["pv"][0] # Choose highest ranked move
+                
+
+                f.write(f"{FEN} | {move}\n") # append the (FEN | optimal_move)
 
 engine.quit()
