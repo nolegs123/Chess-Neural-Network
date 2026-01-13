b6eccf1e9e677ded5e8129438fedbdb61b550b25
nolegs123 <mikkelbaunchristensen@gmail.com>
2026-01-13 14:15:42 +0100

    Har opdelt vores kode i forskellige folders (vi kan altid revert hvis det bliver messy)

--- Chess Neural Network/Symbolic AI/symbolicAI.py
+++ Chess Neural Network/Symbolic AI/symbolicAI.py
@@ -1,44 +1,8 @@
 import chess
 from evaluation import evaluate_board
+from move_sort import order_moves
 
 nodes_searched = 0
-
-def order_moves(board):
-    piece_value = {
-        chess.PAWN: 1,
-        chess.KNIGHT: 3,
-        chess.BISHOP: 3,
-        chess.ROOK: 5,
-        chess.QUEEN: 9,
-        chess.KING: 100
-    }
-
-    captures = []
-    checks = []
-    quiet = []
-
-    for move in board.legal_moves:
-        if board.is_capture(move):
-            captures.append(move)
-        else:
-            board.push(move)
-            if board.is_check():
-                checks.append(move)
-            else:
-                quiet.append(move)
-            board.pop()
-
-    captures.sort(
-        key=lambda m: piece_value.get(
-            board.piece_at(m.to_square).piece_type
-            if board.piece_at(m.to_square) else 0,
-            0
-        ),
-        reverse=True
-    )
-
-    return captures + checks + quiet
-
 
 def minimax(board, depth, alpha, beta):
     global nodes_searched
