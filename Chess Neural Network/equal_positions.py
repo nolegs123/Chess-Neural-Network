8639ee91e5da86b3c8bc4f81f7447c6456fbb9ff
nolegs123 <mikkelbaunchristensen@gmail.com>
2026-01-13 10:38:40 +0100

    den tager kun unikke (ved ikke hvordan man staver til det) positioner nu

--- Chess Neural Network/equal_positions.py
+++ Chess Neural Network/equal_positions.py
@@ -2,27 +2,41 @@ import chess.engine
 import chess.pgn
 
 pgn_file = r"C:\Users\mikke\Downloads\lichess_elite_2021-08\lichess_elite_2021-08.pgn"
-engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\mikke\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe")
+engine = chess.engine.SimpleEngine.popen_uci(
+    r"C:\Users\mikke\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe"
+)
+
 desired_fens = 100
 
 pgn = open(pgn_file)
 found_fens = 0
+seen_positions = set()
 
-while True:
-    if found_fens >= desired_fens:
+while found_fens < desired_fens:
+    game = chess.pgn.read_game(pgn)
+    if game is None:
         break
 
-    game = chess.pgn.read_game(pgn)
     board = chess.Board()
+
     for i, move in enumerate(game.mainline_moves()):
         board.push(move)
-        if found_fens >= desired_fens:
-            break
+
         if i == 20:
             info = engine.analyse(board, chess.engine.Limit(depth=12))
-            score = info["score"].pov(board.turn)
-            score = score.score(mate_score=10000)
+            score = info["score"].pov(board.turn).score(mate_score=10000)
+
+            if score is None:
+                break
+
             if -26 <= score <= 26:
-                with open("equal_positions/equal_positions.txt", "a") as f:
-                    f.write(f"{board.fen()} \n")
+                fen = board.fen()
+
+                if fen not in seen_positions:
+                    with open("equal_positions/equal_positions.txt", "a") as f:
+                        f.write(f"{fen}\n")
+
+                    seen_positions.add(fen)
                     found_fens += 1
+
+            break
