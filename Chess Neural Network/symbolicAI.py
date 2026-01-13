65fee47b69f719ea9b6804fbaae7fc06febe938c
nolegs123 <mikkelbaunchristensen@gmail.com>
2026-01-13 14:03:38 +0100

    Har opdelt symbolic AI filen for at være mere læselig ved at tilføje en fil til evaluation

--- Chess Neural Network/symbolicAI.py
+++ Chess Neural Network/symbolicAI.py
@@ -1,30 +1,7 @@
 import chess
+from evaluation import evaluate_board
 
 nodes_searched = 0
-
-def evaluate_board(board) -> int:
-    pieces = {
-        chess.PAWN: 1,
-        chess.KNIGHT: 3,
-        chess.BISHOP: 3,
-        chess.ROOK: 5,
-        chess.QUEEN: 9
-    }
-
-    if board.is_checkmate():
-        return float('-inf') if board.turn == chess.WHITE else float('inf')
-
-    if board.is_stalemate() or board.is_insufficient_material() \
-        or board.can_claim_threefold_repetition() or board.can_claim_fifty_moves():
-        return 0
-
-    evaluation = 0
-    for piece_type, value in pieces.items():
-        evaluation += len(board.pieces(piece_type, chess.WHITE)) * value
-        evaluation -= len(board.pieces(piece_type, chess.BLACK)) * value
-
-    return evaluation
-
 
 def order_moves(board):
     piece_value = {
