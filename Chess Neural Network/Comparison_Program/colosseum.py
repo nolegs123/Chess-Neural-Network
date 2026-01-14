e8b64a164b512eef14ef26f7af5825f11fdcdf1b
MischievousDegenerate <runersc@outlook.com>
2026-01-14 12:22:20 +0100

    new

--- Chess Neural Network/Comparison_Program/colosseum.py
+++ Chess Neural Network/Comparison_Program/colosseum.py
@@ -4,9 +4,15 @@ import chess
 
 model_path = "r\"C:\\Users\\mikke\\source\\repos\\Chess-Neural-Network\\Chess Neural Network\\Data\\testing_data\\\equal_positions\\equal_positions.txt\""
 
+black_wins = 0
+white_wins = 0
+draw = 0
+
 with open(model_path, "r") as file:
     for FEN in file:
         board = chess.Board(FEN)
+
+        
 
         
     
