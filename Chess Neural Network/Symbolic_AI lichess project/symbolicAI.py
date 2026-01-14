27f2042ee62e8840527b4b72e890eb6f0baeab31
l-fuji-l <daggabriel@gmail.com>
2026-01-14 13:56:41 +0100

    Symbolic Ai lichess project started

---
+++ Chess Neural Network/Symbolic_AI lichess project/symbolicAI.py
@@ -0,0 +1,76 @@
+import chess
+import random
+from Symbolic_AI.evaluation import evaluate_board
+from Symbolic_AI.move_sort import order_moves
+
+nodes_searched = 0
+
+def minimax(board, depth, alpha, beta):
+    global nodes_searched
+    nodes_searched += 1
+
+    if depth == 0 or board.is_game_over():
+        return evaluate_board(board)
+
+    if board.turn == chess.WHITE:
+        best_eval = float('-inf')
+        for move in order_moves(board):
+            board.push(move)
+            eval = minimax(board, depth - 1, alpha, beta)
+            board.pop()
+
+            best_eval = max(best_eval, eval)
+            alpha = max(alpha, best_eval)
+
+            if beta <= alpha:
+                break
+
+        return best_eval
+
+    else:
+        best_eval = float('inf')
+        for move in order_moves(board):
+            board.push(move)
+            eval = minimax(board, depth - 1, alpha, beta)
+            board.pop()
+
+            best_eval = min(best_eval, eval)
+            beta = min(beta, best_eval)
+
+            if beta <= alpha:
+                break
+
+        return best_eval
+
+
+def get_best_move(board, depth=5):
+    global nodes_searched
+    nodes_searched = 0
+
+    best_move = None
+    best_eval = float('-inf') if board.turn == chess.WHITE else float('inf')
+
+    alpha = float('-inf')
+    beta = float('inf')
+
+    for move in order_moves(board):
+        board.push(move)
+        eval = minimax(board, depth - 1, alpha, beta)
+        board.pop()
+
+        if board.turn == chess.WHITE:
+            if eval >= best_eval:
+                best_eval = eval
+                best_move = move
+                alpha = max(alpha, best_eval)
+        else:
+            if eval <= best_eval:
+                best_eval = eval
+                best_move = move
+                beta = min(beta, best_eval)
+
+    if best_move == None:
+        print(f"NO MOVE FOUND. RETURNING RANDOM. \n {board.fen()}")
+        best_move = random.choice(order_moves(board))
+
+    return best_move, nodes_searched
