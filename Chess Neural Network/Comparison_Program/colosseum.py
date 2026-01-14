9bea6b4f6e523a00b2fc7e00b24ea6f8b76aa03d
MischievousDegenerate <runersc@outlook.com>
2026-01-14 11:23:58 +0100

    update

--- Chess Neural Network/Comparison_Program/colosseum.py
+++ Chess Neural Network/Comparison_Program/colosseum.py
@@ -2,8 +2,11 @@ import chess
 
 # GET EQUAL POSITIONS
 
-model_path = ""
-file = ""
+model_path = "r\"C:\\Users\\mikke\\source\\repos\\Chess-Neural-Network\\Chess Neural Network\\Data\\testing_data\\\equal_positions\\equal_positions.txt\""
 
-for FEN in file:
-    board = chess.Board()
+with open(model_path, "r") as file:
+    for FEN in file:
+        board = chess.Board(FEN)
+
+        
+    
