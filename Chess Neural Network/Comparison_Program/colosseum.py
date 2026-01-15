f931e30111853835353399285ead7fa27ee64c4f
MischievousDegenerate <runersc@outlook.com>
2026-01-15 10:14:01 +0100

    new update 15/1/2026

--- Chess Neural Network/Comparison_Program/colosseum.py
+++ Chess Neural Network/Comparison_Program/colosseum.py
@@ -6,9 +6,9 @@ import Symbolic_AI.symbolicAI as symAI
 
 equal_file = os.path.join("equal_positions", "equal_positions.txt")
 
-black_wins = 0
-white_wins = 0
-draw = 0
+nn_wins = 0
+symAI_wins = 0
+draws = 0
 
 with open(equal_file, "r") as file:
     for i, FEN in enumerate(file):
@@ -32,3 +32,20 @@ with open(equal_file, "r") as file:
                 print(board)
 
         print(board.result())
+        if board.result() == "1-0":
+            if nn_color == chess.WHITE:
+                nn_wins += 1
+            else:
+                symAI_wins += 1
+        elif board.result() == "0-1":
+            if nn_color == chess.BLACK:
+                symAI_wins += 1
+            else:
+                nn_wins += 1
+        else:
+            draw += 1
+
+        print("My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions, " \
+        "loyal servant to the true emperor, Marcus Aurelius. Father to a murdered son, husband to a murdered wife. And I will have my vengeance, "
+        "in this life or the next")
+        print(f"Final Score of the colosseum: Neural Network Wins: {nn_wins}, Symbolic AI Wins: {symAI_wins}, Draws: {draws}")
