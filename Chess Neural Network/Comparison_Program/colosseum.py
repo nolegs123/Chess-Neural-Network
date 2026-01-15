6dd9d8ff28e0d77fb8a35ee73b41f07dc53fde85
nolegs123 <mikkelbaunchristensen@gmail.com>
2026-01-15 13:23:10 +0100

    runes commit

--- Chess Neural Network/Comparison_Program/colosseum.py
+++ Chess Neural Network/Comparison_Program/colosseum.py
@@ -76,19 +76,19 @@ with open(equal_file, "r") as file:
         with open(pgn_path, "w", encoding="utf-8") as pgn_file:
             print(game, file=pgn_file)
 
-    # print final results after all games are played
-    if board.result() == "1-0":
-        if nn_color == chess.WHITE:
-            nn_wins += 1
+        # print final results after all games are played
+        if board.result() == "1-0":
+            if nn_color == chess.WHITE:
+                nn_wins += 1
+            else:
+                symAI_wins += 1
+        elif board.result() == "0-1":
+            if nn_color == chess.BLACK:
+                symAI_wins += 1
+            else:
+                nn_wins += 1
         else:
-            symAI_wins += 1
-    elif board.result() == "0-1":
-        if nn_color == chess.BLACK:
-            symAI_wins += 1
-        else:
-            nn_wins += 1
-    else:
-        draws += 1
+            draws += 1
         
     print("My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions, " \
     "loyal servant to the true emperor, Marcus Aurelius. Father to a murdered son, husband to a murdered wife. And I will have my vengeance, "
