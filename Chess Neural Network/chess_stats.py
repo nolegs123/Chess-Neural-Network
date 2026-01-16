cdb632d5a3a26fb8ee96cda773bd06c9a6f154b0
MischievousDegenerate <runersc@outlook.com>
2026-01-16 13:07:12 +0100

    confidence interval of the difference

--- Chess Neural Network/chess_stats.py
+++ Chess Neural Network/chess_stats.py
@@ -3,11 +3,12 @@ import numpy as np
 import scipy.stats as stats
 
 #%% Sample size calculation for two proportions for power analysis
+# Pilot study results (n=30 games):
 
 alpha = 0.01    # chance of false positive
 beta = 0.01     # chance of false negative
 p1 = 9.5/30     # NN baseline/pilot study win rate
-p2 = 20.5/30    # Symbolic AI baseline/pilot study win win rate
+p2 = 20.5/30    # Symbolic AI baseline/pilot study win rate
 
 
 n = ((stats.norm.ppf(alpha/2) + stats.norm.ppf(beta))**2 * (p1 * (1 - p1) + p2 * (1 - p2))) / (p1 - p2)**2
@@ -21,3 +22,15 @@ alpha = 0.05  # significance level
 
 n = stats.norm.ppf(alpha/2)**2 * (p1 * (1 - p1) + p2 * (1 - p2)) / ME**2
 print(f"Sample size for specific ME: {n}")
+
+#%% Confidence interval of the difference in proportions
+
+p1 = (12 + 28 * 0.5) / 78  # NN wins + half draws
+p2 = (38 + 28 * 0.5) / 78  # Symbolic AI wins + half draws
+
+alpha = 0.05
+diff = p2 - p1
+lower = diff - stats.norm.ppf(1-alpha/2) * np.sqrt((p1 * (1 - p1) / 78) + (p2 * (1 - p2) / 78))
+upper = diff + stats.norm.ppf(1-alpha/2) * np.sqrt((p1 * (1 - p1) / 78) + (p2 * (1 - p2) / 78))
+
+print(f"Confidence interval of the difference: [{lower:.4f}, {upper:.4f}]")
