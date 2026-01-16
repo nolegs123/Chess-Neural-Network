1749c2657a591fda48bf6da3ff32967203a92757
MischievousDegenerate <runersc@outlook.com>
2026-01-16 10:42:42 +0100

    NEW STATS CALCULATIONS

--- Chess Neural Network/chess_stats.py
+++ Chess Neural Network/chess_stats.py
@@ -1,21 +1,23 @@
+#%% Library imports
 import numpy as np
 import scipy.stats as stats
 
-# Sample size calculation for two proportions for power analysis
+#%% Sample size calculation for two proportions for power analysis
 
-alpha = 0.05    # chance of false positive
-beta = 0.05     # chance of false negative
-p1 = 7/30
-p2 = 14/30
+alpha = 0.01    # chance of false positive
+beta = 0.01     # chance of false negative
+p1 = 9.5/30     # NN baseline/pilot study win rate
+p2 = 20.5/30    # Symbolic AI baseline/pilot study win win rate
 
 
 n = ((stats.norm.ppf(alpha/2) + stats.norm.ppf(beta))**2 * (p1 * (1 - p1) + p2 * (1 - p2))) / (p1 - p2)**2
 
 print(f"Sample size: {n}")
 
-# Sample size calculation for two proportions for specific confidence interval
+#%% Sample size calculation for two proportions for specific confidence interval
 
 ME = 0.03  # margin of error
+alpha = 0.05  # significance level
 
 n = stats.norm.ppf(alpha/2)**2 * (p1 * (1 - p1) + p2 * (1 - p2)) / ME**2
 print(f"Sample size for specific ME: {n}")
